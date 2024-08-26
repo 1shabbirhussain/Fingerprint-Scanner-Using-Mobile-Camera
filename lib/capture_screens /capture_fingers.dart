1d5a47898a2645f006e88d186510e49882941bf8
Shabbir Hussain <001.shabbirhussain@gmail.com>
2024-08-26 20:17:07 +0500

    ignore suggestion

--- lib/capture_screens /capture_fingers.dart
+++ lib/capture_screens /capture_fingers.dart
@@ -7,6 +7,7 @@ class FingerCapturePage extends StatefulWidget {
   const FingerCapturePage({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _FingerCapturePageState createState() => _FingerCapturePageState();
 }
 
